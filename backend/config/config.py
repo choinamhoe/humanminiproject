495ba4ab097135859b7bdbd3fe40a428cb03ff92
unknown <cnh665@gmail.com>
2025-09-10 13:52:56 +0900

    250910 choinamhoe cors 호출 경로를 임시로 일단 모두 접근하게 수정

--- backend/config/config.py
+++ backend/config/config.py
@@ -15,10 +15,10 @@ config = {
     }
 }
 cors_config = {
-    "allow_origins":[
-        f"http://localhost:{config['FRONT_PORT']}"
-        ],
-    #  "allow_origins":["*"],
+    # "allow_origins":[
+    #     f"http://localhost:{config['FRONT_PORT']}"
+    #     ],
+    "allow_origins":["*"],
     "allow_credentials":True,
     "allow_methods":["*"],
     "allow_headers":["*"],
